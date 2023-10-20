441dade15212e96fa264fdf6345b8ead01f71264
chx38895 <116450342+chx38895@users.noreply.github.com>
2023-10-20 23:59:16 +0200

    Update test.js

--- test.js
+++ test.js
@@ -107,7 +107,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const dbConfig = {
-  host: "moritodb.mysql.database.azure.com",
+  host: `process.env.DB_HOST`,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
